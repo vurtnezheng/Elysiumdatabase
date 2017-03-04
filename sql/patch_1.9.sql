a6747223656b4d1773e155df1b5a65b47e1b09a2
shenna <shenna.elysium@gmail.com>
2017-03-04 22:02:48 +0300

    Patch 1.9 / Disabling Spell "Savage Guard"

--- sql/patch_1.9.sql
+++ sql/patch_1.9.sql
@@ -8,6 +8,10 @@ REPLACE INTO `spell_mod` (`Id`, `AttributesEx3`, `Comment`) VALUES
 (24383, 1048576, 'Swiftness of Zanza - persist through death (before 1.12)');
 DELETE FROM `spell_disabled` WHERE `entry` = 24417;
 
+-- Disabling Spell "Savage Guard"
+DELETE FROM `spell_disabled` WHERE `entry` = 28161;
+INSERT INTO `spell_disabled` VALUES (28161);
+
 -- Enabling Disabled Spells
 DELETE FROM spell_disabled WHERE entry IN (25117, 25118, 25119, 25120, 25123, 25122, 25121, 25660, 25704, 25722, 25804, 25691, 26276);
 
