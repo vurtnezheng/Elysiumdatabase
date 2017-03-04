eb940c49c73303d3f67907dc85d4360e9f93ef33
shenna <shenna.elysium@gmail.com>
2017-03-05 01:17:01 +0300

    Patch 1.9 / Adding Tailoring Recipes to NPC "Mishta" in Cenarion Hold

--- sql/patch_1.9.sql
+++ sql/patch_1.9.sql
@@ -163,6 +163,39 @@ REPLACE `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `group
 REPLACE `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) values('9032','22528','8','0','2','4','70');
 REPLACE `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) values('9032','321234','100','1','-321234','1','0');
 
+-- Adding Tailoring Recipes to NPC "Mishta" in Cenarion Hold
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','2320','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','2321','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','2325','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','2604','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','2678','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','2692','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','2880','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','2901','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','2928','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','3371','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','3372','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','3466','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','3713','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','3857','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','4289','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','4291','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','4340','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','4341','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','4342','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','4399','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','4400','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','5956','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','6217','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','6256','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','6530','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','6532','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','7005','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','8343','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','8925','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','14341','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','18256','0','0');
+REPLACE `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES ('15179','22310','0','0');
 
 -- Disable unavailable instances
 -- ATTENTION! Requires update once events are implemented!
