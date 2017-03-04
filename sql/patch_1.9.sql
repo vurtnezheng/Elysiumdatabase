699552a63ef53fab6aa43171711533e6f1082103
shenna <shenna.elysium@gmail.com>
2017-03-04 20:50:59 +0300

    Patch 1.9 / Changing Random Content Availability

--- sql/patch_1.9.sql
+++ sql/patch_1.9.sql
@@ -7,7 +7,6 @@ REPLACE INTO `spell_mod` (`Id`, `AttributesEx3`, `Comment`) VALUES
 (24417, 1048576, 'Sheen of Zanza - persist through death (before 1.12)'),
 (24383, 1048576, 'Swiftness of Zanza - persist through death (before 1.12)');
 DELETE FROM `spell_disabled` WHERE `entry` = 24417;
-DELETE FROM `spell_disabled` WHERE `entry` IN (25117, 25118, 25119, 25120, 25123, 25122, 25121, 25660, 25704, 25722, 25804, 25691, 26276);
 
 -- Spawn of Azuregos, Kazzak 1.3
 UPDATE `creature` SET `spawnFlags` = 0 WHERE id IN (6109, 12397);
@@ -20,6 +19,39 @@ UPDATE `game_event` SET `disabled` = 1 WHERE `entry` IN
 160,		-- 1.10 Patch : t0.5 Quest Chain (unchecked)
 167,		-- Patch 1.10
 168 		-- Patch 1.11 ? 1.12
+);
+
+-- Enable available game events
+UPDATE `game_event` SET `disabled` = 0 WHERE `entry` IN
+(
+14, 15, 40,			-- Stranglethorn Fishing Extravaganza 1.7
+4, 5, 100, 101, 	-- DarkMoon Faire 1.6
+13,					-- Elemental Invasion 1.5
+16, 38, 39,			-- Gurubashi Arena 1.5
+18,					-- Call to arms: Alterac Valley 1.5
+156, 159,			-- Dire Maul extra content(unchecked) 1.3
+35, 36, 37,
+42, 43, 44,			-- The Maul 1.3
+66,					-- Dragons of Nightmare 1.8
+161,				-- Patch 1.3
+162,				-- Patch 1.4 | 1.5
+163,				-- Patch 1.6
+164,				-- Patch 1.7
+165,				-- Patch 1.8
+154,				-- Silithus @ 1.9 (unchecked)
+166					-- Patch 1.9
+);
+
+-- Enable Alterac Valley 1.5
+UPDATE `battleground_template` SET `MinLvl` = 51, `MaxLvl` = 60 WHERE `id` = 1;
+
+-- Disable unavailable instances
+-- ATTENTION! Requires update once events are implemented!
+UPDATE `areatrigger_teleport` SET `required_level` = 61 WHERE `target_map` IN 
+(
+533,    -- Naxxramas 1.12
+531,    -- AQ40 (Temple of AQ) 1.9
+509	    -- AQ20 (Ruins of AQ) 1.9
 );
 
 -- Enable available instances
