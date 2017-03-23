57fa4a4c396eb40c572ba16f2a15d3c28beb422d
eir <m-laniakea@users.noreply.github.com>
2017-03-23 15:21:16 -0700

    Patch 1.3 / Make Treasure of Shen'Dralar completeable

--- sql/1.03/1.03_gameobjects.sql
+++ sql/1.03/1.03_gameobjects.sql
@@ -2,3 +2,7 @@
 -- Remove Unused DM GO
 
 DELETE FROM `gameobject` WHERE `id` IN (179674, 179673, 179672);
+
+-- Add involvedrelation for Treasure of Shen'dralar
+INSERT INTO `gameobject_involvedrelation` (`id`, `quest`) VALUES
+(179517,    7877);
