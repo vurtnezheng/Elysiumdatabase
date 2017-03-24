21e9754460da428a9b71626cc97524edb26c8487
shenna <shenna.elysium@gmail.com>
2017-03-24 18:53:39 +0300

    Patch 1.3 / Dire Maul GO Update

--- sql/1.03/1.03_creatures.sql
+++ sql/1.03/1.03_creatures.sql
@@ -2,3 +2,7 @@
 -- Enable spawn of Azuregos & Kazzak (1.3)
 
 UPDATE `creature` SET `spawnFlags` = 0 WHERE id IN (6109, 12397);
+
+-- Remove Warlock Epic Mount QUest Trigger
+
+DELETE FROM `creature` WHERE `id` = 1000001;
