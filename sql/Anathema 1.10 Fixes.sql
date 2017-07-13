e680148022542d0f1499a372bb939ddea5657ae7
shenna <akirashirase90@gmail.com>
2017-07-14 01:04:59 +0300

    templame ^.6
    creature_templame
    reature_templame
    eature_templame
    ature_templame
    ture_templame
    ure_templame
    re_templame
    e_templame
    _templame
    templame

--- sql/Anathema 1.10 Fixes.sql
+++ sql/Anathema 1.10 Fixes.sql
@@ -82,7 +82,7 @@ UPDATE `creature_template` SET `spell1` = 17547, `spell2` = 20252, `spell3` = 19
 -- Malgen's Aimed Shot, Frost Trap and Pet, Gnashjaw
 UPDATE `creature_template` SET `spell2` = 20902, `spell4` = 13809, `PetSpellDataId` = 19561 WHERE `entry` = 16052;
 -- NPC Bodley EquipID
-UPDATE creature_templame SET equipment_id = 16033 WHERE entry = 16033;
+UPDATE creature_template SET equipment_id = 16033 WHERE entry = 16033;
 DELETE FROM `creature_equip_template` WHERE entry = 16033;
 INSERT INTO `creature_equip_template` (`entry`, `equipentry1`, `equipentry2`, `equipentry3`) VALUES (16033, 13069, 0, 0);
 -- Wrong quest ordering
