cb0ffb4885fff49f564d66e2f95590f1b90f6570
shenna <akirashirase90@gmail.com>
2017-07-16 23:16:07 +0300

    Itemization for human beings

---
+++ updates/1.12 Patch Fixes.sql
@@ -0,0 +1,27 @@
+-- 1.12 Patch Updates
+
+-- * INDEX
+-- - 1. ENCOUNTER				 
+-- - 2. EVENTS 					          
+-- - 3. ITEMS AND LOOT		 	      
+-- - 4. VENDORS 				
+-- - 5. CREATURES				
+-- - 6. QUESTS					
+-- - 7. PROFESSIONS				
+-- - 8. SPELLS			 		
+-- - 9. GAMEOBJECTS		
+
+-- * ENCOUNTER
+
+-- * EVENTS 
+
+-- * ITEMS AND LOOT
+
+-- * VENDORS
+
+-- * CREATURES
+
+-- * QUESTS
+
+-- * PROFESSIONS 
+
