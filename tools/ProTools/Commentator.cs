b0e8d880e06d8b69c625293f22ad061a61e2b664
Josi <tito.elysium@gmail.com>
2017-08-06 03:46:24 +0200

    Made the "commentator" a bit more useful
    ...by storing each item_template inside a dictionary, instead of an array.

--- tools/ProTools/Commentator.cs
+++ tools/ProTools/Commentator.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Commentator
 {
     class Program
     {
         /// <summary>
-        /// Adds a comment to each statement
+        /// 
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
@@ -34,14 +35,11 @@ namespace Commentator
             {
                 Console.WriteLine($"Checking line: {line}");
 
-                // First, we save all the item's stats inside an array
-                var item = line.Split(new string[] { ") VALUES (" }, StringSplitOptions.None)[1].Replace(");", "").Replace("'", "").Split(new string[] { ", " }, StringSplitOptions.None);
-
-                // Next, we get the individual values that we want to append in the comment
-                var name = item[3].Replace(@"\", "");
+                // First, we save all the item's stats inside a dictionary
+                var item = GetItemTemplate(line);
 
                 // Finally, we create the comment and add it AND the SQL statement to the list
-                result.Add($"-- {name}");
+                result.Add($"-- {item["name"]}");
                 result.Add(line);
             }
 
@@ -49,7 +47,30 @@ namespace Commentator
             Console.WriteLine($"Finished saving to: {args[1]}");
             File.AppendAllLines(args[1], result);
             Console.WriteLine("DONE!");
-            Console.ReadKey();
+        }
+
+        private static Dictionary<string, string> GetItemTemplate(string line)
+        {
+            var item = new Dictionary<string, string>();
+            var keys = line.Split(new string[] { ") VALUES (" }, StringSplitOptions.None)[0]
+                .Replace("REPLACE INTO `item_template` (", "")
+                .Replace("`", "").Split(new string[] { ", " }, StringSplitOptions.None);
+            var values = Regex.Replace(line.Split(new string[] { ") VALUES (" }, StringSplitOptions.None)[1]
+                .Replace(");", "")
+                .Replace("'", "")
+                .Replace(@"\", ""), "[a-zA-Z], [a-zA-Z]", delegate (Match match) 
+                {
+                    return match.ToString().Replace(" ", "<REGEX>");
+                })
+                .Split(new string[] { ", " }, StringSplitOptions.None);
+            values[3] = values[3].Replace("<REGEX>", " ");
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                item.Add(keys[i], values[i]);
+            }
+
+            return item;
         }
     }
 }
