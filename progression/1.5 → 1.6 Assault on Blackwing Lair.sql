c415667dfa5bb806d711b229d16dc7632e2dfb63
shenna <akirashirase90@gmail.com>
2017-08-07 17:52:21 +0300

    Dummy SQLs for new progression patcher

---
+++ progression/1.5 → 1.6 Assault on Blackwing Lair.sql
@@ -0,0 +1,100 @@
+-- 1.5 → 1.6 | Assault on Blackwing Lair
+
+DROP TABLE IF EXISTS `new_items`;
+DROP TABLE IF EXISTS `new_events`;
+DROP TABLE IF EXISTS `new_quests`;
+DROP TABLE IF EXISTS `new_creatures`;
+
+CREATE TABLE IF NOT EXISTS `new_quests` (entry mediumint PRIMARY KEY);
+CREATE TABLE IF NOT EXISTS `new_items` (entry mediumint PRIMARY KEY);
+CREATE TABLE IF NOT EXISTS `new_events` (entry mediumint PRIMARY KEY);
+CREATE TABLE IF NOT EXISTS `new_creatures` (entry mediumint PRIMARY KEY);
+
+-- * INDEX
+-- - 1.  NEW ENCOUNTER				 
+-- - 2.  NEW EVENTS 					          
+-- - 3.  NEW ITEMS 		  
+-- - 4.  ITEM STATS	
+-- - 5.  LOOT TABLES	 	      
+-- - 6.  VENDORS 
+-- - 7.  NEW CREATURES			
+-- - 8.  CREATURES	
+-- - 9.  NEW QUESTS			
+-- - 10. QUEST CHANGES					
+-- - 11. PROFESSIONS
+-- - 12. NEW SPELLS				
+-- - 15. NEW GAMEOBJECTS	
+
+-- * NEW ENCOUNTER	
+
+UPDATE `areatrigger_teleport` SET `required_level` = 61 WHERE `target_map` = 429; -- Dire Maul 1.3
+
+
+-- * NEW EVENTS 	
+
+REPLACE INTO new_events SELECT entry FROM game_event WHERE entry IN (1000
+
+);
+
+-- * NEW ITEMS 	
+
+REPLACE INTO new_items SELECT entry FROM item_template WHERE entry IN (1000
+
+);
+ 
+-- * ITEM STATS | Items changed: 
+
+	
+-- * LOOT TABLES
+
+
+-- * VENDORS 
+
+
+-- * NEW CREATURES
+
+REPLACE INTO new_creatures SELECT entry FROM creature_template WHERE entry IN (1000
+
+);
+		
+-- * CREATURES
+
+	
+-- * NEW QUESTS	
+
+REPLACE INTO new_quests SELECT entry FROM quest_template WHERE entry IN (1000
+
+);
+
+-- * QUEST CHANGES	
+
+
+-- * PROFESSIONS
+
+
+-- * NEW SPELLS	
+
+DELETE FROM spell_disabled WHERE entry IN (1000);
+			
+-- * NEW GAMEOBJECTS	
+
+-- * RESTORATION TOOLS
+
+REPLACE INTO creature_loot_template SELECT * FROM creature_loot_template_full WHERE item IN (SELECT entry FROM `new_items`);
+REPLACE INTO reference_loot_template SELECT * FROM reference_loot_template_full WHERE item IN (SELECT entry FROM `new_items`);
+
+REPLACE INTO creature_loot_template SELECT c.* FROM creature_loot_template_full c INNER JOIN reference_loot_template_full r ON c.item = r.entry WHERE r.item IN (SELECT entry FROM `new_items`);
+REPLACE INTO disenchant_loot_template SELECT * FROM disenchant_loot_template_full WHERE item IN (SELECT entry FROM `new_items`); 
+REPLACE INTO fishing_loot_template SELECT * FROM fishing_loot_template_full WHERE item IN (SELECT entry FROM `new_items`); 
+REPLACE INTO gameobject_loot_template SELECT * FROM gameobject_loot_template_full WHERE item IN (SELECT entry FROM `new_items`); 
+REPLACE INTO item_loot_template SELECT * FROM item_loot_template_full WHERE item IN (SELECT entry FROM `new_items`); 
+REPLACE INTO mail_loot_template SELECT * FROM mail_loot_template_full WHERE item IN (SELECT entry FROM `new_items`); 
+REPLACE INTO pickpocketing_loot_template SELECT * FROM pickpocketing_loot_template_full WHERE item IN (SELECT entry FROM `new_items`); 
+REPLACE INTO skinning_loot_template SELECT * FROM skinning_loot_template_full WHERE item IN (SELECT entry FROM `new_items`); 
+REPLACE INTO npc_vendor SELECT * FROM npc_vendor_full WHERE item IN (SELECT entry FROM `new_items`); 
+
+UPDATE `quest_template` SET `Method` = (`Method` | 2) WHERE `entry` IN (SELECT * FROM `new_quests`);
+
+UPDATE `creature` SET `spawnFlags` = (`spawnFlags` | 0) WHERE id IN (SELECT entry FROM new_creatures);
+
+UPDATE `game_event` SET `disabled` = (`disabled` | 0) WHERE entry IN (SELECT entry FROM new_events);
